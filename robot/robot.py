8882169176929434778510a8273f0005f16f7d48
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-01-18 19:32:33 -0500

    Update robot.py

--- robot/robot.py
+++ robot/robot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import wpilib
+from commandbased import CommandBasedRobot
+from networktables import NetworkTables
 
-class Robot(wpilib.IterativeRobot):
+class Robot(CommandBasedRobot):
 
     def robotInit(self):
         pass
