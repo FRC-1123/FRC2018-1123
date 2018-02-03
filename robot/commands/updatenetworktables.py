798ef1e9e4fad6f75c22b647c59da2993cdf9eb1
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-02-03 18:17:11 -0500

    Add commands

---
+++ robot/commands/updatenetworktables.py
@@ -0,0 +1,24 @@
+import wpilib
+from networktables import NetworkTables
+from wpilib.command import Command
+
+import subsystems
+
+class UpdateNetworkTables(Command):
+
+	def __init__(self):
+		super().__init__("Update NetworkTables")
+
+        self.sd = NetworkTables.getTable("SmartDashboard")
+        self.nt_timer = wpilib.Timer()  # timer for updating NetworkTables
+
+    def initialize(self):
+        self.nt_timer.start()
+
+    def execute(self):
+        if self.nt_timer.hasPeriodPassed(0.1):  # update NetworkTables every 0.1 seconds
+            # dashboard forward button (for testing purposes)
+            if self.sd.containsKey("forwardCommand") and self.sd.getBoolean("forwardCommand"):  # check if move forward button pressed
+                self.sd.putBoolean("forwardCommand", False) # set button back 
+                DriveForward(24).start()
+                #self.logger.info("Moving forward at half power for one second.")
