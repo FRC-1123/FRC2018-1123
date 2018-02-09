aaed8e80b12a4d846d721ae66e2f55bb95873ff1
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-02-08 19:54:37 -0500

    Add placeholder for OI

--- robot/robotmap.py
+++ robot/robotmap.py
@@ -13,3 +13,12 @@ motors.right_front_id = 2
 motors.right_back_id = 3
 
 cameras = PropertySet()
+
+left_joystick = PropertySet()
+left_joystick.port = 0
+
+right_joystick = PropertySet()
+right_joystick.port = 1
+
+xbox_controller = PropertySet()
+xbox_controller.port = 2
