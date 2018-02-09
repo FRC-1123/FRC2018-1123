ecd2db3e46193e0512998c374815b0ccc095d05d
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-02-08 20:44:35 -0500

    Update Robot.py

--- robot/robot.py
+++ robot/robot.py
@@ -5,20 +5,27 @@ from commandbased import CommandBasedRobot
 from commands.respondtocontroller import RespondToController
 from commands.updatenetworktables import UpdateNetworkTables
 from networktables import NetworkTables
+from inputs import oi
 
 import subsystems
 
 class Robot(CommandBasedRobot):
 
     def robotInit(self):
-    	subsystems.init()
+        subsystems.init()
+
+        self.sd = NetworkTables.getTable("SmartDashboard")
+        self.ds = wpilib.DriverStation.getInstance()
+        self.game_data = ""
+        oi.init()
 
     def autonomousInit(self):
-    	pass
+        self.game_data = self.ds.getGameSpecificMessage()
+
 
     def teleopInit(self):
         RespondToController().start()
         UpdateNetworkTables().start()
 
 if __name__ == "__main__":
-	wpilib.run(Robot)
+    wpilib.run(Robot)
