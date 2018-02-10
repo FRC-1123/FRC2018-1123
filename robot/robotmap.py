8acb8d2963cb2f8acd4e1bc53796d895340971ec
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-02-10 16:06:43 -0500

    Start lift mech motors

--- robot/robotmap.py
+++ robot/robotmap.py
@@ -12,6 +12,10 @@ motors.left_back_id = 1
 motors.right_front_id = 2
 motors.right_back_id = 3
 
+lift_mech = PropertySet()
+lift_mech.motor_a = 4
+lift_mech.motor_b = 5
+
 cameras = PropertySet()
 
 joystick = PropertySet()
--- robot/subsystems/liftmech.py
+++ robot/subsystems/liftmech.py
@@ -1,12 +1,25 @@
+import ctre
+import wpilib
+from networktables import NetworkTables
 from wpilib.command.subsystem import Subsystem
+from ctre.wpi_talonsrx import TalonSRX
+from wpilib.drive import DifferentialDrive
+
+import robotmap
 
 
 class LiftMech(Subsystem):
     """
     This subsystem will control the lift mechanism
-    Details TBD
+    It will use two motors, which will be at ~half power, and in 
+    opposite directions.
     """
 
     def __init_(self):
         super().__init__("LiftMech")
-        
+
+        self.motor_a = TalonSRX(robotmap.lift_mech.motor_a)
+        self.motor_a.setInverted(False)
+
+        self.motor_b = TalonSRX(robotmap.lift_mech.motor_b)
+        self.motor_b.setInverted(True)
