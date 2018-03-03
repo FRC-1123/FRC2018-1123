6b78b2380d64c8e87a0fd7490e8fa5c3c8685a8f
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-03-03 16:47:38 -0500

    Update grabber to use talons

--- robot/subsystems/grabber.py
+++ robot/subsystems/grabber.py
@@ -2,8 +2,7 @@ import ctre
 import wpilib
 from networktables import NetworkTables
 from wpilib.command.subsystem import Subsystem
-from wpilib import Relay
-#from ctre.wpi_talonsrx import WPI_TalonSRX as Talon
+from ctre.wpi_talonsrx import WPI_TalonSRX as Talon
 
 import robotmap
 
@@ -13,23 +12,33 @@ class Grabber(Subsystem):
     one for each side of the grabber and associated rollers. 
     """
     GRABBER_IN, GRABBER_OUT, GRABBER_IDLE = range(3)
+    DEFAULT_SPEED = 0.6 # 60% power
 
     def __init__(self):
         super().__init__("Grabber")
 
-        self.left_motor = Relay(robotmap.intake.left_motor, Relay.Direction.kBoth) #Talon(robotmap.intake.left_motor)
-        self.right_motor = Relay(robotmap.intake.right_motor, Relay.Direction.kBoth) #Talon(robotmap.intake.right_motor)
+        self.left_motor = Talon(robotmap.intake.left_motor)
+        self.right_motor = Talon(robotmap.intake.right_motor)
 
+        self.left_motor.setInverted(False)
+        self.left_motor.setInverted(True)
+
+    ## For legacy purposes; if this method is used, will just go at 60% speed
     def set_mode(self, grab_mode):
     	if grab_mode == Grabber.GRABBER_IN:
-    		self.left_motor.set(Relay.Value.kForward)
-    		self.right_motor.set(Relay.Value.kReverse)
+    		self.left_motor.set(Grabber.DEFAULT_SPEED)
+    		self.right_motor.set(Grabber.DEFAULT_SPEED)
     	elif grab_mode == Grabber.GRABBER_OUT:
-    		self.left_motor.set(Relay.Value.kReverse)
-    		self.right_motor.set(Relay.Value.kForward)
+    		self.left_motor.set(-Grabber.DEFAULT_SPEED)
+    		self.right_motor.set(-Grabber.DEFAULT_SPEED)
     	else:
-    		self.left_motor.set(Relay.Value.kOff)
-    		self.right_motor.set(Relay.Value.kOff)
+    		self.left_motor.set(0.0)
+    		self.right_motor.set(0.0)
 
-        #self.left_motor.setInverted(False)
-        #self.right_motor.setInverted(True)
+    def set_grab_speed(self, grab_speed, inverted=False):
+        if not inverted:
+            self.left_motor.set(grab_speed)
+            self.right_motor.set(grab_speed)
+        else:
+            self.left_motor.set(-grab_speed)
+            self.right_motor.set(-grab_speed)
